3b89765eb9e3a0de6d630176dd14a1862cd69854
BenQuirk <benquirk@yahoo.com>
2015-05-22 12:17:33 -0400

    style tweaks

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 
 
+
+// write code that changes background color of UL to none if it's empty
+// OR changes backcolor of UL to 0,0,0,.3 if it's not empty
+// if LI exists on page, then $('ul').css = (background, 0,0,0,.3)
+
+
+
+
+
+
 var usersArray = []; // must declare outside of the scope of the function below
 
 
@@ -41,7 +51,7 @@ $('form').submit(function(e) {
   var userName = usersArray[usersArray.length-1].firstNameKey +' '+ usersArray[usersArray.length-1].lastNameKey;
   var userEmail = usersArray[usersArray.length-1].emailKey;
 
-  var listUser = $('<li>').append('<span class="name">'+userName+'</span><span class="email">'+userEmail+'</span>');
+  var listUser = $('<li class="list-item">').append('<span class="name">'+userName+'</span><span class="email">'+userEmail+'</span>');
   $('#todo-list').append(listUser);
 
 
